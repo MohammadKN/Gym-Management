e2667eb2e97a2e1e9c86aa567953e011a8252a24
MohammadKN <Mohdkn.15@gmail.com>
2025-01-16 23:32:07 +0300

    menus

--- Gym-Management/Gym-Management.cpp
+++ Gym-Management/Gym-Management.cpp
@@ -9,66 +9,13 @@
 
 using namespace std;
 
-void showClientMenu(string username) {
-	int choice;
-
-	do {
-		cout << "Gym Management System\n";
-		cout << "Hello " << username << " How Was Your Day" << endl;
-		cout << "1. Access Schedule\n";
-		cout << "2. Communicate with Trainer\n";
-		cout << "3. Communicate with Nutrition Specialist\n";
-		cout << "4. Logout\n";
-		cout << "5. Exit\n";
-		cout << "Enter your choice: ";
-		Schedule s(0, 1, 3, "hello", "hi", 2, 5);
-		cin >> choice;
-		switch (choice) {
-		case 1:
-			system("cls");
-			s.displaySchedule();
-			system("cls");
-
-			break;
-		case 2:
-			
-			break;
-		case 3:
-			cout << "Exiting...\n";
-			break;
-		default:
-			cout << "Invalid choice. Try again.\n";
-		}
-	} while (choice != 3);
-
-}
-void showTrainerMenu(string username) {
-	cout << "Gym Management System\n";
-	cout << "Hello "<< username << " How Was Your Day" << endl;
-    cout << "1. Manage Schedules\n";
-    cout << "2. Communicate with a Client\n";
-	cout << "3. Logout\n";
-    cout << "4. Exit\n";
-    cout << "Enter your choice: ";
-}
-
-void showNutritionSpecialistMenu(string username) {
-	cout << "Gym Management System\n";
-	cout << "Hello "<< username << " How Was Your Day" << endl;
-	cout << "1. Create a Diet Plan\n";
-    cout << "1. Manage Clients' Diet Plans\n";
-    cout << "2. Communicate with a Client\n";
-	cout << "3. Logout\n";
-    cout << "4. Exit\n";
-    cout << "Enter your choice: ";
-}
-
-
+bool loggedIn = false;
 map<string, string> clients;
 map<string, string> trainers;
 map<string, string> nutritionSpecialists;
 
 void loadUsers() {
+
 	ifstream infile("clients.txt");
 	string username, password;
 	while (infile >> username >> password) {
@@ -108,7 +55,129 @@ void saveUsers() {
 	}
 	outfile.close();
 }
+void displayUsers() {
+	cout << "Clients:\n";
+	for (const auto& user : clients) {
+		cout << "Username: " << user.first << ", Password: " << user.second << endl;
+	}
 
+	cout << "\nTrainers:\n";
+	for (const auto& user : trainers) {
+		cout << "Username: " << user.first << ", Password: " << user.second << endl;
+	}
+
+	cout << "\nNutrition Specialists:\n";
+	for (const auto& user : nutritionSpecialists) {
+		cout << "Username: " << user.first << ", Password: " << user.second << endl;
+	}
+}
+
+void showAdminDashboard() {
+	int choice;
+	do {
+		cout << "Gym Management System\n";
+		cout << "1. Display All Users\n";
+		cout << "2. Show Encrypted Data\n";
+		cout << "3. Back\n";
+		cout << "Enter your choice: ";
+		
+		cin >> choice;
+		system("cls");
+		switch (choice) {
+		case 1:
+			displayUsers();
+			break;
+		case 2:
+			int table[MAX];
+			initializeTable(table);
+
+			hashings(table, 5);
+			hashings(table, 15);
+			hashings(table, 25);
+
+			displays(table);
+			break;
+		case 3:
+			cout << "Going Back...\n";
+			break;
+		default:
+			cout << "Invalid choice. Try again.\n";
+		}
+	} while (choice != 5);
+}
+
+void showClientMenu(string username) {
+	int choice;
+
+	do {
+		cout << "Gym Management System\n";
+		cout << "Hello " << username << " How Was Your Day" << endl;
+		cout << "1. Access Schedule\n";
+		cout << "2. Logout\n";
+		cout << "Enter your choice: ";
+		Schedule s(0, 1, 3, "hello", "hi", 2, 5);
+		cin >> choice;
+		switch (choice) {
+		case 1:
+			system("cls");
+			s.displaySchedule();
+			system("cls");
+			break;
+		case 2:
+			loggedIn = false;
+			break;
+		default:
+			cout << "Invalid choice. Try again.\n";
+		}
+	} while (choice != 3);
+
+}
+void showTrainerMenu(string username) {
+	int choice;
+
+	do {
+		cout << "Gym Management System\n";
+		cout << "Hello " << username << " How Was Your Day" << endl;
+		cout << "1. Create a Schedule\n";
+		cout << "2. Logout\n";
+		cout << "Enter your choice: ";
+		Schedule s(0, 1, 3, "hello", "hi", 2, 5);
+		cin >> choice;
+		switch (choice) {
+		case 1:
+			system("cls");
+			s.displaySchedule();
+			system("cls");
+			break;
+		case 2:
+			loggedIn = false;
+			break;
+		default:
+			cout << "Invalid choice. Try again.\n";
+		}
+	} while (choice != 3);
+}
+
+void showNutritionSpecialistMenu(string username) {
+	cout << "Gym Management System\n";
+	cout << "Hello "<< username << " How Was Your Day" << endl;
+	cout << "1. Create a Diet Plan\n";
+    cout << "1. Manage Clients' Diet Plans\n";
+    cout << "2. Communicate with a Client\n";
+	cout << "3. Logout\n";
+    cout << "4. Exit\n";
+    cout << "Enter your choice: ";
+}
+
+
+
+void showAuth() {
+	cout << "Gym Management System\n";
+	cout << "1. Login\n";
+	cout << "2. Register\n";
+	cout << "3. Exit\n";
+	cout << "Enter your choice: ";
+}
 
 bool loginMenu() {
 	string username, password;
@@ -121,25 +190,48 @@ bool loginMenu() {
 	cout << "1. Client\n";
 	cout << "2. Trainer\n";
 	cout << "3. Nutrition Specialist\n";
+	cout << "4. Admin\n";
 	cout << "Enter your choice: ";
 	int accountType;
 	cin >> accountType;
 	system("cls");
 	loadUsers();
-	if (clients.find(username) != clients.end() && clients[username] == password) {
-		cout << "Login successful!\n";
-		if (username == "Client") {
+	cout << clients[username];
+	switch (accountType) {
+	case 1:
+		if (clients.find(username) != clients.end() && clients[username] == password) {
+			cout << "Login successful!\n";
 			showClientMenu(username);
-		} else if (username == "Trainer") {
-			showTrainerMenu(username);
-		} else if (username == "Nutrition") {
-			showNutritionSpecialistMenu(username);
+			return true;
 		}
-		return true;
-	} else {
+		break;
+	case 2:
+		if (trainers.find(username) != trainers.end() && trainers[username] == password) {
+			cout << "Login successful!\n";
+			showTrainerMenu(username);
+			return true;
+		}
+		break;
+	case 3:
+		if (nutritionSpecialists.find(username) != nutritionSpecialists.end() && nutritionSpecialists[username] == password) {
+			cout << "Login successful!\n";
+			showNutritionSpecialistMenu(username);
+			return true;
+		}
+		break;
+	case 4:
+		if (username == "admin" && password == "admin") {
+			cout << "Login successful!\n";
+			showAdminDashboard();
+			return true;
+		}
+		break;
+	default:
 		cout << "Invalid username or password. Try again.\n";
 		return false;
+		
 	}
+	
 }
 
 
@@ -186,27 +278,18 @@ bool registerMenu() {
 
 
 int main() {
-	
-    int choice;
-	int table[MAX];
-	initializeTable(table);
-
-	hashings(table, 5);
-	hashings(table, 15);
-	hashings(table, 25);
-
-	displays(table);
+	int choice;
     do {
         showAuth();
         cin >> choice;
 		system("cls");
         switch (choice) {
-            case 1: if (!loginMenu()) showAuth(); break;
-            case 2: registerMenu(); break;
+			case 1: if (!loginMenu()) showAuth(); else loggedIn = true; break;
+			case 2: registerMenu(); loggedIn = true; break;
             case 3: cout << "Exiting...\n"; break;
             default: cout << "Invalid choice. Try again.\n";
         }
-    } while (choice != 3);
+    } while (choice != 4 && !loggedIn);
 
     return 0;
 }
